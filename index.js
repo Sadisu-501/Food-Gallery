0f35b43219dd5b572ca54bf5b91989d2d5988e9e
Saskid-501 <Sadisukelvin@gmail.com>
2026-08-09 16:50:51 +0100

    card contents and styles

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const btn = document.getElementById ("btn");
-const links = document.getElementById ("menu-links");
+const btn = document.getElementById("btn");
+const links = document.getElementById("menu-links");
 const overlay = document.querySelector(".overlay");
 
 btn.addEventListener("click", () => {
   links.classList.toggle("hidden");
   overlay.classList.toggle("hidden");
+});
+
+overlay.addEventListener("click", () => {
+  links.classList.add("hidden");
+  overlay.classList.add("hidden");
 });
