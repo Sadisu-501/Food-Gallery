95fa2e8fc5c00d14ec5200cfd9c7052e0a645d4e
Saskid-501 <Sadisukelvin@gmail.com>
2026-08-09 14:29:54 +0100

    main cards contents

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const btn = document.getElementById("btn");
-const links = document.getElementById("menu-links");
+const btn = document.getElementById ("btn");
+const links = document.getElementById ("menu-links");
+const overlay = document.querySelector(".overlay");
 
 btn.addEventListener("click", () => {
   links.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
 });
